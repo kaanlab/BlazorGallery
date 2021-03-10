e5f4e587c0f544d11533212a0db1ad860bc7ffbf
kaanlab <kaan@live.ru>
2021-03-10 15:02:34 +0300

    Initial commit

---
+++ BlazorGallery/Models/Picture.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BlazorGallery.Models
+{
+    public class Picture
+    {
+        public string ImgUrl { get; set; }
+        public string Text { get; set; }
+        public string VideoUrl { get; set; }
+        public string Author { get; set; }
+    }
+}
